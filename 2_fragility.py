591b351fda29192ebbef1e58fa1d3eb0b90e4fd7
ErikinBC <alla_hack@hotmail.com>
2021-08-26 17:01:47 -0400

    updating fragility functions

--- 2_fragility.py
+++ 2_fragility.py
@@ -1,7 +1,11 @@
+# Script to calculate RI for each study in sheet
+
+# Load modules
 import os
+import numpy as np
 import pandas as pd
 import scipy.stats as stats
-import funs_fragility as fi
+from funs_fragility import FI_func
 
 dir_base = os.getcwd() 
 dir_output = os.path.join(dir_base,'output')
@@ -9,25 +13,60 @@ dir_output = os.path.join(dir_base,'output')
 df_FI = pd.read_csv(os.path.join(dir_output,'df_FI.csv'))
 df_inf = pd.read_csv(os.path.join(dir_output,'df_inf.csv'))
 
+##############################
+# ----- (1) Sanity Check --- #
 
-####################################
-# ----- CALCULATE FI + P-VALUE --- #
+# Type-I error rate
+alpha = 0.05
+
+# Forward and backward shuold get the same result conditional on test/swap
+n1A, n1, n2A, n2 = 160, 200, 50, 100
+forward1 = FI_func(n1A, n1, n2A, n2, 'fisher', 1, mode='forward', alpha=alpha)
+backward1 = FI_func(n1A, n1, n2A, n2, 'fisher', 1, mode='backward', alpha=alpha)
+forward2 = FI_func(n1A, n1, n2A, n2, 'fisher', 2, mode='forward', alpha=alpha)
+backward2 = FI_func(n1A, n1, n2A, n2, 'fisher', 2, mode='backward', alpha=alpha)
+
+assert np.all(np.array(forward1['tbl_FI']) == np.array(backward1['tbl_FI']))
+assert np.all(np.array(forward2['tbl_FI']) == np.array(backward2['tbl_FI']))
+
+
+
+##############################
+# ----- (2) Calculate FI --- #
+
+vec_test = np.repeat(['fisher','chi2','chi2_cont'],2)
+vec_swap = np.tile([1,2],3)
 
 holder = []
 for ii, rr in df_FI.iterrows():
-    n1, n1a, n2, n2a = rr['num1'], rr['num_out1'], rr['num2'], rr['num_out2']
-    n1b, n2b = n1 - n1a, n2 - n2a
-    tab = [[n1a,n1b], [n2a,n2b]]
-    if (ii + 1) % 50 == 0:
-        print('Study %i of %i (n=%i)' % (ii+1, df_FI.shape[0], n1+n2))
-    # ---- Baseline significance ---- #
-    if (n1a==0) & (n2a==0):
-        print('Zero events happened for either group')
-        pval_FI, pval_chi, pval_chi2 = 1, 1, 1
-    else:
-        pval_FI, pval_chi, pval_chi2 = stats.fisher_exact(tab)[1], \
-                            stats.chi2_contingency(tab,correction=True)[1], \
-                            stats.chi2_contingency(tab,correction=False)[1]
+    n1A, n1, n2A, n2 = rr['num_out1'], rr['num1'], rr['num_out2'], rr['num2']
+    print('Study %i of %i (n=%i)' % (ii+1, df_FI.shape[0], n1+n2))
+    # (i) Fisher
+    fisher_1 = FI_func(n1A, n1, n2A, n2, 'fisher', 1, None, None, None, 0.05)
+    fisher_2 = FI_func(n1A, n1, n2A, n2, 'fisher', 2, None, None, None, 0.05)
+    # (ii) Chi2
+    chi2_1_False = FI_func(n1A, n1, n2A, n2, 'chi2', 1, None, None, None, 0.05, False)
+    chi2_2_False = FI_func(n1A, n1, n2A, n2, 'chi2', 2, None, None, None, 0.05, False)
+    # (iii) Chi2 + Continuity
+    chi2_1_True = FI_func(n1A, n1, n2A, n2, 'chi2', 1, None, None, None, 0.05, True)
+    chi2_2_True = FI_func(n1A, n1, n2A, n2, 'chi2', 2, None, None, None, 0.05, True)
+    # (iv) Combine and store
+    lst_res = [fisher_1, fisher_2, chi2_1_False, chi2_2_False, chi2_1_True, chi2_2_True]
+    vec_FI = [lst['FI'] for lst in lst_res]
+    vec_pval = [lst['pv_bl'] for lst in lst_res]
+    res = pd.DataFrame({'test':vec_test, 'swap':vec_swap,'FI':vec_FI,'pval':vec_pval})
+    res.insert(0,'ID',rr['ID'])
+    res.insert(0,'tt',rr['tt'])
+
+
+    # # ---- Baseline significance ---- #
+    # if (n1A==0) & (n2A==0):
+    #     print('Zero events happened for either group')
+    #     pval_FI, pval_chi, pval_chi2 = 1, 1, 1
+    # else:
+    #     pval_FI, pval_chi, pval_chi2 = stats.fisher_exact(tab)[1], \
+    #                         stats.chi2_contingency(tab,correction=True)[1], \
+    #                         stats.chi2_contingency(tab,correction=False)[1]
     # ---- Insignificant results ---- #
     if rr['tt'] == 'neg': # reverse fragility
         assert pval_FI > 0.05
--- funs_fragility.py
+++ funs_fragility.py
@@ -2,130 +2,140 @@ import numpy as np
 import scipy.stats as stats
 from support_funs import stopifnot
 
-###############################################################################
-# ------------------------ FRGARILITY INDEX FUNCTIONS ----------------------- # 
-###############################################################################
+# Wrappers for different p-value approaches
+def pval_fisher(tbl, *args):
+  return stats.fisher_exact(tbl,*args)[1]
 
-# Function to calculate the fragility index
-#      n1A : Number of patients in group1 with primary outcome
-#      n1 : Total number of patients in group1
-#      n2A : Number of patients in group2 with primray outcome
-#      n2 : Total of patients in group2
+def pval_chi2(tbl, *args):
+  return stats.chi2_contingency(tbl,*args)[1]
 
-# Returns a dictionary
-#      FI: the fragility index
-#      group: whether patients were added to group A or B
-#      pv_bl: the baseline p-value from the Fisher exact test
-#      pv_FI: the infimum of non-signficant p-values
+def vprint(stmt, bool):
+  if bool:
+    print(stmt)
 
-def fi_func(n1A, n1, n2A, n2):
-  n1B = n1 - n1A
-  n2B = n2 - n2A
-  stopifnot((n1B >= 0) & (n2B >= 0),'A exceeds A+B')
-  prop1, prop2 = n1A / (n1A + n1B), n2A / (n2A + n2B)
-  bl_pval = stats.fisher_exact([[n1A, n1B], [n2A, n2B]])[1]
-  fi, fi_pval, ii = 0, 0, 0 # Initialize
-  if bl_pval > 0.05: # Check that baseline result is actually significant
-    di_ret = {'FI':fi,'group':'NA','pv_bl':bl_pval,'pv_FI':bl_pval}
+
+##############################################################################
+# ------------------------ FRAGILITY INDEX FUNCTIONS ----------------------- # 
+##############################################################################
+
+"""
+input: integers of the following
+n1A:      Number of patients in group1 with primary outcome
+n1:       Total number of patients in group1
+n2A:      Number of patients in group2 with primray outcome
+n2:       Total of patients in group2
+statfun:  "fisher" or "chi2"
+mode:     Whether while loop should begin at data (forward) or equality (backward)
+swap:     Whether group1 or group2 should have the patients swapped
+n1B:      Can be specified is n1 is None
+n2B:      Can be specified is n2 is None
+*args:    Will be passed into statsfun
+
+output: a dictionary with the following keys
+FI:       The fragility index
+ineq:     Whether group1 had a proportion less than or greater than group2
+swap:     Whether events were swapped from group1 or group2
+pv_bl:    The baseline p-value from the Fisher exact test
+pv_FI:    The infimum of non-signficant p-values
+"""
+
+# n1A, n1, n2A, n2, n1B, n2B = 480, 860, 180, 430, None, None
+# statfun, swap, mode, alpha, args = 'fisher', 1, 'forward', 0.05, ()
+def FI_func(n1A, n1, n2A, n2, statfun='fisher', swap=1, mode=None, n1B=None, n2B=None, alpha=0.05, *args):
+  assert (swap == 1) or (swap == 2)
+  assert (statfun == 'fisher') or (statfun == 'chi2')
+  if statfun == 'fisher':
+    pval_fun = pval_fisher
   else:
-    while fi_pval < 0.05:
-      ii += 1
-      if prop1 < prop2:
-        n1A += 1
-        n1B += -1
-      else:
-        n2A += 1
-        n2B += -1
-      fi_pval = stats.fisher_exact([[n1A, n1B], [n2A, n2B]])[1]      
-    di_ret = {'FI':ii, 'group':np.where(prop1<prop2,['A'],['B'])[0],
-              'pv_bl':bl_pval, 'pv_FI':fi_pval}
+    pval_fun = pval_chi2
+  if (n1B is None) or (n2B is None):
+    assert (n1 is not None) and (n2 is not None)
+    n1B = n1 - n1A
+    n2B = n2 - n2A
+  else:
+    assert (n1B is not None) and (n2B is not None)
+    n1 = n1A + n1B
+    n2 = n2A + n2B
+  lst_int = [n1A, n1, n2A, n2, n1B, n2B]
+  assert all([isinstance(i,int) for i in lst_int])
+  assert (n1B >= 0) & (n2B >= 0)
+  prop1, prop2 = n1A / n1, n2A / n2
+  # Calculate the baseline p-value
+  tbl_bl = [[n1A, n1B], [n2A, n2B]]
+  bl_pval = pval_fun(tbl_bl, *args)
+  # Determine if we should add or subtract from n1A
+  sign1 = int(np.where(prop1 > prop2, -1, +1))
+  sign2 = int(np.where(prop1 > prop2, +1, -1))
+  # Determine which group should be modified
+  if swap == 1:
+    sign2 = 0
+  if swap == 2:
+    sign1 = 0
+  ineq = str(np.where(prop1==prop2, 'eq', np.where(prop1>prop2, 'gt', 'lt')))
+  if mode is None:
+    mode = 'forward'
+    if n1 + n2 > 2000:
+      mode = 'backward'
+  # Initialize FI and p-value
+  di_ret = {'FI':0, 'ineq':ineq, 'swap':swap, 'pv_bl':bl_pval, 'pv_FI':bl_pval, 'tbl_bl':tbl_bl, 'tbl_FI':tbl_bl}
+  if bl_pval < alpha:  # Check that baseline result is actually significant
+    FI, pval, tbl_FI = find_FI(n1A, n1B, n2A, n2B, sign1, sign2, swap, pval_fun, mode, alpha, *args)
+    # Update dictionary
+    di_ret['FI'] = FI
+    di_ret['pv_FI'] = pval
+    di_ret['tbl_FI'] = tbl_FI
   return di_ret
 
-
-def fi_func2(n1A, n1, n2A, n2):
-  n1B = n1 - n1A
-  n2B = n2 - n2A
-  stopifnot((n1B >= 0) & (n2B >= 0),'A exceeds A+B')
-  prop1, prop2 = n1A / (n1A + n1B), n2A / (n2A + n2B)
-  bl_pval = stats.fisher_exact([[n1A, n1B], [n2A, n2B]])[1]
-  fi, fi_pval, ii = 0, 0, 0 # Initialize
-  if bl_pval > 0.05: # Check that baseline result is actually significant
-    di_ret = {'FI':fi,'group':'NA','pv_bl':bl_pval,'pv_FI':bl_pval}
+# Back end function to perform the for-loop
+def find_FI(n1A, n1B, n2A, n2B, sign1, sign2, swap, pval_fun, mode='forward', alpha=0.05, *args):
+  is_bool = False
+  assert (mode == 'forward') or (mode == 'backward')
+  n1a, n1b, n2a, n2b = n1A, n1B, n2A, n2B
+  n1, n2 = n1A + n1B, n2A + n2B
+  if mode == 'forward':
+    FI, pval = 0, 0
+    while pval < alpha:
+      FI += 1
+      vprint('Step %i of %i' % (FI, max(n1-n1A,n1-n1B)), is_bool)
+      n1a += sign1 * 1
+      n1b += sign1 * -1
+      n2a += sign2 * 1
+      n2b += sign2 * -1
+      tbl = [[n1a, n1b], [n2a, n2b]]
+      pval = pval_fun(tbl, *args)
   else:
-    while fi_pval < 0.05:
-      ii += 1
-      if prop1 > prop2:
-        n1A += -1
-        n1B += +1
-      else:
-        n2A += -1
-        n2B += +1
-      tab = [[n1A, n1B], [n2A, n2B]]
-      fi_pval = stats.fisher_exact(tab)[1]      
-    di_ret = {'FI':ii, 'group':np.where(prop1<prop2,['A'],['B'])[0],
-              'pv_bl':bl_pval, 'pv_FI':fi_pval}
-  return di_ret
-
-def fi_func_yates(n1A, n1, n2A, n2):
-  n1B = n1 - n1A
-  n2B = n2 - n2A
-  stopifnot((n1B >= 0) & (n2B >= 0),'A exceeds A+B')
-  prop1, prop2 = n1A / (n1A + n1B), n2A / (n2A + n2B)
-  bl_pval = stats.chi2_contingency([[n1A, n1B], [n2A, n2B]],correction=False)[1]
-  fi, fi_pval, ii = 0, 0, 0 # Initialize
-  if bl_pval > 0.05: # Check that baseline result is actually significant
-    di_ret = {'FI':fi,'group':'NA','pv_bl':bl_pval,'pv_FI':bl_pval}
-    return(di_ret)
-  while fi_pval < 0.05:
-    ii += 1
-    if prop1 < prop2:
-      n1A += 1
-      n1B += -1
+    prop1 = n1A / n1
+    prop2 = n2A / n2
+    if swap == 1:
+      n1a = int(n1 * prop2)
+      n1b = n1 - n1a
     else:
-      n2A += 1
-      n2B += -1
-    fi_pval = stats.chi2_contingency([[n1A, n1B], [n2A, n2B]],correction=False)[1]
-  di_ret = {'FI':ii, 'group':np.where(prop1<prop2,['A'],['B'])[0],
-            'pv_bl':bl_pval, 'pv_FI':fi_pval}
-  return(di_ret)
+      n2a = int(n2 * prop1)
+      n2b = n2 - n2a
+    tbl = [[n1a, n1b], [n2a, n2b]]
+    pval_test = pval_fun(tbl, *args)
+    rFI = 0
+    while pval_test > alpha:
+      rFI += 1
+      vprint('Step %i of %i' % (rFI, max(n1-n1A,n1-n1B)), is_bool)
+      tbl_test = [[n1a-sign1, n1b+sign1], [n2a-sign2, n2b+sign2]]
+      pval_test = pval_fun(tbl_test, *args)
+      if pval_test > alpha:
+        n1a, n1b, n2a, n2b = tbl_test[0] + tbl_test[1]
+        tbl = [[n1a, n1b], [n2a, n2b]]
+        pval = pval_fun(tbl, *args)
+    # Reverse calculate FI
+    if swap == 1:
+      FI = sign1*(n1a-n1A)
+    else:
+      FI = sign2*(n2a-n2A)
+  assert pval > alpha
+  return FI, pval, tbl
 
 
-# --- EQUIVALENT FUNCTION FOR REVERSE COUNTER --- #
-def fi_func_rev(n1A, n1, n2A, n2):
-  n1B = n1 - n1A
-  n2B = n2 - n2A
-
-  prop1 = n1A / (n1A + n1B)
-  prop2 = n2A / (n2A + n2B)
-  if prop1 < prop2:
-    tbl = np.array([[n1A, n1B], [n2A, n2B]])
-  else:
-    tbl = np.array([[n2A, n2B], [n1A, n1B]])
-  bl_pval = stats.fisher_exact(tbl)[1] # Calculate baseline p-value
-  if bl_pval > 0.05:
-    di_ret = {'FI':0,'group':'NA','pv_bl':bl_pval,'pv_FI':bl_pval}
-    return(di_ret)
-  # Start at equal proportion and decrease
-  tbl_FI = tbl.copy()
-  tbl_FI[0,0] = int(np.floor(tbl_FI[0].sum()*max(prop1,prop2)))
-  tbl_FI[0,1] = tbl[0].sum() - tbl_FI[0,0]
-  fi_pval = stats.fisher_exact(tbl_FI)[1]
-  ii = 0
-  while fi_pval > 0.05:
-    ii += 1
-    tbl_FI[0,0] += -1
-    tbl_FI[0, 1] += 1
-    fi_pval = stats.fisher_exact(tbl_FI)[1]
-  # We need ii - 1 steps
-  fi = (tbl_FI[0,0]+1) - tbl[0,0]
-  fi_pval = stats.fisher_exact(tbl_FI + [[1,-1],[0,0]])[1]
-  # Return
-  di_ret = {'FI':fi, 'group':np.where(prop1<prop2,['A'],['B'])[0],
-          'pv_bl':bl_pval, 'pv_FI':fi_pval}
-  return(di_ret)
 
 # --- FOR CALCULATING INSIFICANT RESULTS --- #
-def fi_func_neg(n1A, n1, n2A, n2):
+def fi_func_neg(n1A, n1, n2A, n2, alpha=0.05):
   n1B, n2B = n1 - n1A, n2 - n2A
   prop1, prop2  = n1A / (n1A + n1B), n2A / (n2A + n2B)
   # If both proportions < 50%, add to largest
@@ -152,11 +162,11 @@ def fi_func_neg(n1A, n1, n2A, n2):
           k = -1
           tbl = tbl[[1,0]]
   bl_pval = stats.fisher_exact(tbl)[1] # baseline p-value
-  stopifnot(bl_pval  > 0.05,'woah sig result!')
+  stopifnot(bl_pval  > alpha,'woah sig result!')
   tbl_FI = tbl.copy()
   fi_pval = 1
   ii = 0
-  while fi_pval > 0.05:
+  while fi_pval > alpha:
     ii += 1
     tbl_FI[0,0] += k
     tbl_FI[0, 1] += -k
